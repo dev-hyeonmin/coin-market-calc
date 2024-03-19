4b4dea060bdb77ff47de7a4665166f0d304c5f6a
dev-hyeonmin <dev.hyeonmin@gmail.com>
2024-03-19 12:11:14 +0900

    Success Calc

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { useEffect, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
-import { Coin } from "./type";
+import { Coin, Quotes } from "./type";
+import { fetchHistoricalData, formattedDate } from "./utils";
 
 type FormProps = {
-  coin: number;
+  coin: string;
   day: number;
   percent: number;
-  period: string;
+  startDate: string;
+  endDate: string;
 }
 
 function App() {
-  const [_, setTopCoins] = useState<Coin[]>([]);
+  const [topCoins, setTopCoins] = useState<Coin[]>([]);
+  const [totalCount, setTotalCount] = useState(0);
 
   useEffect(() => {
     // 상위 200위의 코인 불러오기
@@ -29,21 +32,113 @@ function App() {
     watch,
     formState: { errors },
   } = useForm<FormProps>()
-  const onSubmit: SubmitHandler<FormProps> = (data) => console.log(data)
+  const onSubmit: SubmitHandler<FormProps> = async (data) => {
+    const COUNT = 365;
+    const selectedCoinId = data.coin;
+    const yValue = Number(data.percent) * 0.01;
+    const xDaysAgo = Number(data.day);
+
+    let startDate = new Date(data.startDate);
+    let endDate = new Date(data.endDate);
+
+    let allQuotes: Quotes[] = []; // 종가 리스트
+    let caseCount = 0;
+    let futureProfitsArray: number[][] = Array.from({ length: 14 }, () => []); // 평균 수익률
+    let winRates: number[][] = Array.from({ length: 14 }, () => []); // 승률
+    
+    // const selectedCoin = topCoins.find(coin => {
+    //   return coin.id == selectedCoinId;
+    // });
+
+    // if (!selectedCoin) {
+    //   console.log(`⚠️ ${selectedCoinId} : Coint Not Found.`);
+    //   return;
+    // }
+
+    // 구간이 전체 기간인지, 특정 날짜 기간인지 확인
+    if (data.startDate === "전체기간") {
+      // startDate = new Date(selectedCoin.date_added); // coin.dateAdded
+      startDate = new Date('2020-04-10');
+    }
+
+    let date = startDate;
+    do {
+      // await new Promise((resolve) => setTimeout(resolve, 1000)); // delay      
+      const dateFormat = formattedDate(startDate);
+      const historicalList = await fetchHistoricalData(selectedCoinId, dateFormat, COUNT);
+      const quotes = historicalList?.quotes;
+
+      console.log(`Fetch Coin / ${selectedCoinId} : ${dateFormat}`);
+
+      if (quotes) {
+        allQuotes = [...allQuotes, ...quotes];
+      }
+
+      startDate.setDate(date.getDate() + 365); // next date
+      // break;
+    } while (startDate < endDate)
+
+
+    allQuotes?.map((data, index) => {
+      if (index - xDaysAgo < 0) {
+        return;
+      }
+
+      // const todayDate = data.time_close.substring(0, 10);
+      const todayClose = Number(data.quote.USD.close);
+      const xDaysAgoClose = Number(allQuotes[index - xDaysAgo]?.quote.USD.close);
+
+      const profit = (todayClose / xDaysAgoClose) - 1; // 과거 수익률
+
+      if (profit > yValue) {
+        caseCount++;
+
+        // 1일 - 14일 이후의 과거 수익률 계산
+        for (let j = 1; j <= 14; j++) {
+          if ((index + j) > allQuotes.length - 1) {
+            return;
+          }
+
+          const futureClose = Number(allQuotes[index + j]?.quote.USD.close);
+          const futureProfit = (futureClose / todayClose) - 1;
+
+          futureProfitsArray[j - 1].push(futureProfit);
+          winRates[j - 1].push(futureProfit > 0 ? 1 : 0);
+        }
+      }
+    });
+
+    setTotalCount(caseCount);
+
+    const profitResult = futureProfitsArray.map(innerArray =>
+      (innerArray.reduce((acc, currentValue) => acc + currentValue, 0) / caseCount * 100).toFixed(2)
+    );
+
+    const winRateResult = winRates.map(innerArray =>
+      (innerArray.reduce((acc, currentValue) => acc + currentValue, 0) / caseCount * 100).toFixed(2)
+    );
+
+    console.log(caseCount);
+    console.log(profitResult);
+    console.log(winRateResult);
+  }
 
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
         <select {...register("coin")}>
-          <option value={74}>Dogecoin</option>
+          <option value={5426}>Solana</option>
         </select>
 
-        <input {...register("day")} defaultValue={1}/>
-        <input {...register("percent")} defaultValue={20}/>
-        <input {...register("period")} />
+        <input {...register("day")} defaultValue={7} />
+        <input {...register("percent")} defaultValue={40} />
+        <input {...register("startDate")} placeholder="startDate" defaultValue={'2021-03-14'}/>
+        <input {...register("endDate")} placeholder="endDate" defaultValue={'2024-03-11'}/>
 
         <button type="submit">계산하기</button>
       </form>
+
+      {totalCount}
     </div>
   )
 }
--- src/type.ts
+++ src/type.ts
@@ -2,4 +2,30 @@ export interface Coin {
   id: string;
   name: string;
   symbol: string;
+  date_added: string;
+}
+
+export interface Ohlcv {
+  close: string;
+  open: string;
+  high: string;
+  low: string;
+  market_cap: string;
+  timestamp: string;
+}
+
+export interface Quote {
+  USD: Ohlcv;
+}
+
+export interface Quotes {
+  quote: Quote;
+  time_close: string;
+}
+
+export interface Historical {
+  id: string;
+  name: string;
+  symbol: string;
+  quotes: Quotes[];
 }
